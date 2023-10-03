b74cc3edb60a0d44d7b893d4e08c20c3547f35c4
JuanMorales11122 <127980958+JuanMorales11122@users.noreply.github.com>
2023-10-03 14:00:09 -0600

    Update Volti_Ampi.ino

--- Volti_Ampi.ino
+++ Volti_Ampi.ino
@@ -6,6 +6,12 @@ float volt;        // variable que almacena el voltaje (0.0 a 25.0)
 float Sensibilidad=0.185; //sensibilidad en Voltios/Amperio para sensor de 5A
 int contador = 0;
 int botonResist = 4;
+  int   Pin_de_medicion = 5; // Pin analogico 5
+  int   R1 = 1000; // Cambiar por el valor de la resistencia conocida (Resistencia de clibracion)
+  long  lectura; // Lectura en el pin analogico 5
+  float Tension_de_trabajo = 5.0; // VCC 
+  float Voltaje_R2; // Valor que calculamos
+  float Resistencia; // Valor que calculamos
 
 void setup() {
   Serial.begin(9600);
@@ -30,7 +36,7 @@ if(contador == 2){
   Serial.println(I,3);
 }
   if(contador == 3){
-    //progra para la resistencia
+  ohimetro;
   }
 }
 
@@ -46,11 +52,28 @@ contador = 3;
 }
 }
 
-/*
-// cambio de escala entre floats
-float fmap(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-*/
- 
+ void ohimetro(){
+      // Leemos el valor en el pin A5
+  lectura=0;
+  for(int i=0;i<5;i++){
+    lectura+=analogRead(Pin_de_medicion);
+  }
+  lectura=trunc(lectura/5);
+  
+  // Colculamos el voltaje en la resistencia desconocida
+  Voltaje_R2=(Tension_de_trabajo/1023.0)*lectura;
+  
+  // Lo imprimimos en el Monitor Serie
+  Serial.print("Voltaje en R2: ");
+  Serial.print(Voltaje_R2,2);
+  Serial.println(" Voltios");
+  
+  //Calculamos la resistencia desconocida con las fórmulas
+  // (V1/R1=V2/R2) y (V1=+5V-V2)
+  Resistencia=R1*Voltaje_R2/(Tension_de_trabajo-Voltaje_R2);
+  Serial.print("La resistencia tiene ");
+  Serial.print(Resistencia,2);
+  Serial.println(" Ohm.");
+  Serial.println();
+  delay(5000);
+  }
