b2e11286938da3675e2d2945e1f803f267caa283
JuanMorales11122 <127980958+JuanMorales11122@users.noreply.github.com>
2023-10-03 15:41:23 -0600

    Update Volti_Ampi.ino

--- Volti_Ampi.ino
+++ Volti_Ampi.ino
@@ -14,12 +14,16 @@ int amperimetro = 3;
   float Tension_de_trabajo = 5.0; // VCC 
   float Voltaje_R2; // Valor que calculamos
   float Resistencia; // Valor que calculamos
+int sensorPin = A5;
+int valorSensor;
+float voltaje, r1, r2;
 
 void setup() {
   Serial.begin(9600);
   pinMode(botonResist,INPUT);
   pinMode(voltimetro,INPUT);
 pinMode(amperimetro,INPUT);
+  r2 = 10000; // R2 = 10KΩ
 }
 
 void loop() {
@@ -49,7 +53,15 @@ if(contador == 2){
   Serial.println(I,3);
 }
   if(contador == 3){
-  ohimetro;
+  valorSensor= analogRead(sensorPin); // Lee el valor del pin analogico A0
+voltaje = (valorSensor*5.0/1023.0); // Calcula el voltaje
+r1 = (voltaje*r2)/(5-voltaje);
+Serial.print("Vout = ");
+Serial.print(voltaje);
+Serial.print("V | R1 = ");
+Serial.print(r1);
+Serial.println("Ω");
+delay(1000);
   }
 }
 
