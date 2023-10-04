e8690f120f5f17ec4b547ff26852564e65f38642
JuanMorales11122 <127980958+JuanMorales11122@users.noreply.github.com>
2023-10-03 19:33:11 -0600

    Update Volti_Ampi.ino

--- Volti_Ampi.ino
+++ Volti_Ampi.ino
@@ -3,7 +3,6 @@ float temp=0.0;
 float r111=10000.0;    //r1 valor
 float r222=100000.0;      //r2 valor
 const int ampi = A1;   // seleccionar la entrada para el sensor
-int entrada;      // variable que almacena el valor raw (0 a 1023)
 int entrada1;      // variable que almacena el valor raw (0 a 1023)
 float volt;        // variable que almacena el voltaje (0.0 a 25.0)
 float Sensibilidad=0.185; //sensibilidad en Voltios/Amperio para sensor de 5A
@@ -11,12 +10,7 @@ int contador = 0;
 int botonResist = 4;
 int voltimetro = 2;
 int amperimetro = 3;
-  int   Pin_de_medicion = 5; // Pin analogico 5
-  int   R1 = 1000; // Cambiar por el valor de la resistencia conocida (Resistencia de clibracion)
-  long  lectura; // Lectura en el pin analogico 5
-  float Tension_de_trabajo = 5.0; // VCC 
-  float Voltaje_R2; // Valor que calculamos
-  float Resistencia; // Valor que calculamos
+
 int sensorPin = A5;
 int valorSensor;
 float voltaje, r1, r2;
@@ -30,6 +24,7 @@ pinMode(amperimetro,INPUT);
 }
 
 void loop() {
+
  int ohmi = digitalRead(botonResist);
 int volti = digitalRead(voltimetro);
 int ampi = digitalRead(amperimetro);
@@ -75,29 +70,3 @@ delay(1000);
   }
 }
 
-
- void ohimetro(){
-      // Leemos el valor en el pin A5
-  lectura=0;
-  for(int i=0;i<5;i++){
-    lectura+=analogRead(Pin_de_medicion);
-  }
-  lectura=trunc(lectura/5);
-  
-  // Colculamos el voltaje en la resistencia desconocida
-  Voltaje_R2=(Tension_de_trabajo/1023.0)*lectura;
-  
-  // Lo imprimimos en el Monitor Serie
-  Serial.print("Voltaje en R2: ");
-  Serial.print(Voltaje_R2,2);
-  Serial.println(" Voltios");
-  
-  //Calculamos la resistencia desconocida con las fórmulas
-  // (V1/R1=V2/R2) y (V1=+5V-V2)
-  Resistencia=R1*Voltaje_R2/(Tension_de_trabajo-Voltaje_R2);
-  Serial.print("La resistencia tiene ");
-  Serial.print(Resistencia,2);
-  Serial.println(" Ohm.");
-  Serial.println();
-  delay(5000);
-  }
